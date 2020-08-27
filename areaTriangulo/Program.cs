b7115b137c134d4ce3b2f8e98c4c3a3e11160383
ItsDavoYT <drc412@gmail.com>
2020-08-27 13:57:14 -0500

    P04

---
+++ areaTriangulo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace areaTriangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           float labase, laaltura;
+           float area;
+
+           Console.WriteLine("Dame la base"); labase = float.Parse(Console.ReadLine());
+           Console.WriteLine("Dame la altura"); laaltura = float.Parse(Console.ReadLine());
+
+           area = labase*laaltura/2;
+           Console.WriteLine($"Un triangulo de base{labase} y altura{laaltura} tiene un area de {area}");
+        }
+    }
+}
---
+++ p04pagatrabajador/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace p04pagatrabajador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int horas;
+            float paga, tasa=0.10f;
+            float impuesto, pagabruta,paganeta;
+            Console.WriteLine("Calculando la paga de un trabajador");
+            Console.WriteLine("Dame el nombre"); nombre = Console.ReadLine();
+            Console.WriteLine("Dame las horas"); horas = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dame la paga"); paga = float.Parse(Console.ReadLine());
+            pagabruta = horas * paga;
+            impuesto = pagabruta * tasa;
+            paganeta = pagabruta - impuesto;
+
+            Console.WriteLine($"El trabajador de nombre {nombre}");
+            Console.WriteLine($"Trabajo {horas} horas");
+            Console.WriteLine($"Con una paga de {paga} pesos");
+            Console.WriteLine($"por lo cual recibe una paga bruta de {pagabruta} pesos");
+            Console.WriteLine($"Esto genera un impuesto de {impuesto} pesos");
+            Console.WriteLine($"Al final llega a su casa con la miserable cantidad de {paganeta} pesos");
+        }
+    }
+}
